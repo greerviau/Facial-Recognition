26fc2146841c02571e883160681a56d01dd64e71
greerviau <gviau2@gmail.com>
2019-10-22 17:58:52 -0400

    Fix test

--- test.py
+++ test.py
@@ -1,16 +1,16 @@
 import cv2, os, time
 import numpy as np 
-import facial_recognition as fr
+from facial_recognition import FacialRecognition
 
 if __name__ == '__main__':
-    fcrc = fr.FacialRecognition()
+    fr = FacialRecognition()
     cap = cv2.VideoCapture(cv2.CAP_DSHOW)
     cap.set(3, 1280)
     cap.set(4, 720)
     while(True):
         ret, frame = cap.read()
 
-        frame, names, faces_frame = fcrc.find_faces(frame, return_face_frame=True)
+        frame, names, faces_frame = fr.find_faces(frame, return_face_frame=True)
 
         if faces_frame is not None:
             cv2.imshow('Faces', faces_frame)
